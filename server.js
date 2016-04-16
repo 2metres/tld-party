392d4b3654651b8502b4da93218c19fca599a07b
Andrew Carr <andrew.carr@betterbills.com>
2016-04-16 17:30:55 +1000

    Configure server

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-import express from "express"
-import fs      from "fs"
-import React   from "react"
+import express     from "express"
+import fs          from "fs"
 
 const app = express()
 
@@ -8,8 +7,8 @@ app.get("/", (req, res) => {
   res.send(fs.readFileSync('./index.html', { encoding: 'utf8' }))
 })
 
-app.get("/static/bundle.js", function(req, res) {
-  res.sendFile("bundle.js", {root: __dirname})
+app.get("/static/bundle.js", (req, res) => {
+  res.sendFile("bundle.js", { root: __dirname })
 })
 
 app.listen(3000)
