f0d8ecc26358c2390f2217cd1b55145a41265570
Andrew Carr <andrew.carr@betterbills.com>
2016-04-25 20:15:56 +1000

    Log the hostname

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 import express           from "express"
 import fs                from "fs"
+import morgan            from "morgan"
 import browserify        from "browserify"
+import URI               from "urijs"
 
 import interpolateValues from "./lib/interpolate-values-transform"
 import domains           from "./config/domains"
 
 const app = express()
 
+app.use(morgan('short'))
+
 app.get("/", (req, res) => {
   res.send(fs.readFileSync('./index.html', { encoding: 'utf8' }))
+
+  console.log(`${Date(Date.UTC())}: ${URI(req).tld()} - ${req.hostname}`)
 })
 
 app.get("/static/bundle.js", (req, res) => {
